344e50544b47a068ca766eb466bdd2e483209ad3
alvaroph <alvaroph@gmail.com>
2026-03-19 14:22:11 +0100

    index i canvis en idioma

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<?php include_once "header.php";?>
+<h1>Benvingut a l'aplicació de gestió de videojocs</h1>
+<p>Aquesta aplicació permet gestionar una col·lecció de videojocs, incloent-hi la possibilitat de crear, llegir, actualitzar i eliminar registres de videojocs.</p>
+<p>Utilitza el menú de navegació per accedir a les diferents funcionalitats de l'aplicació.</p>
+
+<button class="btn btn-primary"><a href="mostrar.php" class="text-light">Mostrar videojocs</a></button>
+<button class="btn btn-success"><a href="insertar.php" class="text-light">Registrar videojoc</a></button>
+<?php include_once "footer.php"; ?>
--- insertar.php
+++ insertar.php
@@ -1,14 +1,14 @@
 <?php include_once "header.php"; ?>
 <div class="row">
     <div class="col-12">
-        <h1>Registrar videojuego</h1>
+        <h1>Registrar videojoc</h1>
         <form action="registrar.php" method="POST">
             <div class="form-group">
                 <label for="nombre">Nombre</label>
                 <input placeholder="Nombre" class="form-control" type="text" name="nombre" id="nombre" required>
             </div>
             <div class="form-group">
-                <label for="descripcion">Descripción</label>
+                <label for="descripcion">Descripció</label>
                 <textarea placeholder="Descripción" class="form-control" name="descripcion" id="descripcion" cols="30" rows="10" required></textarea>
             </div>
             <div class="form-group"><button class="btn btn-success">Guardar</button></div>
